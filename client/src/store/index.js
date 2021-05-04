bbdb163513dab2bfb84f9c105d7abe88b559ed93
goniusman <goniusman400@gmail.com>
2021-05-04 14:47:41 +0600

    redex trace added

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,21 @@
-import { createStore, compose, applyMiddleware } from "redux"
-import rootReducer from './reducers/rootReducer'
-import thunk from 'redux-thunk'
+import { createStore, compose, applyMiddleware } from "redux";
+import rootReducer from "./reducers/rootReducer";
+import thunk from "redux-thunk";
 
-const middleware = [thunk]
+const middleware = [thunk];
 
-const store = createStore(rootReducer, compose(
+const store = createStore(
+  rootReducer,
+  compose(
     applyMiddleware(...middleware),
-    window.navigator.userAgent.includes('Chrome') ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : compose
-))
+    window.navigator.userAgent.includes("Chrome")
+      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__({
+            trace: true,
+            traceLimit: 25 || compose,
+          })
+      : compose
+  )
+);
 
-export default store
+export default store;
